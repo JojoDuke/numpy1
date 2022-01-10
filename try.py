d33c31ccf8848e8b940a104bfc28b93a1c26425e
JojoDuke <jojoamankwa@gmail.com>
2022-01-10 22:55:51 +0000

    yu

--- try.py
+++ try.py
@@ -1,6 +1,11 @@
 import numpy as np
 
-array = np.array([1, 8, 3])
+l = [1,2,3]
+l1 = [4,5,6]
+a1 = np.array(l)
+a2 = np.array(l1)
 
-print(array)
-print(array.dtype)
+##----- Dot Product -----##
+dot = np.dot(a1,a2)
+
+##-----  -----##
